26395a53c9628a99bf742771eb27f2fde66fa9be
manchalajahnavi <jahnavimanchala89@gmail.com>
2018-11-15 15:04:22 +0530

    commit

--- application/views/html/index.php
+++ application/views/html/index.php
@@ -259,9 +259,9 @@
     ============================-->
         <section id="call-to-action" class="wow fadeIn">
             <div class="container text-center">
-                <h3>Call To Action</h3>
+                <h3>Mail To Action</h3>
                 <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a class="cta-btn" href="#contact">Call To Action</a>
+                <a class="cta-btn" href="#contact">Mail To Action</a>
             </div>
         </section><!-- #call-to-action -->
 
